0f57093e00340fa9f3149b49aefd5969f0bb5eb4
Marius <mariusdieckmann90@gmail.com>
2021-04-28 22:19:29 +0200

    fixed missing load api in swagger def; added gw stubs

--- build.sh
+++ build.sh
@@ -13,7 +13,9 @@ git checkout dev
 protoc --proto_path=. --go-grpc_out=../build --go_out=../build --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative api/models/*.proto
 protoc --proto_path=. --go-grpc_out=../build --go_out=../build --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative api/services/*.proto
 
-protoc -I . --openapiv2_out ../build/api/openapi --openapiv2_opt logtostderr=true,allow_merge=true api/services/*Service.proto
+protoc -I. --grpc-gateway_out=logtostderr=true,allow_delete_body=true,paths=source_relative:../build api/services/*Service.proto api/services/ObjectLoad.proto
+
+protoc -I . --openapiv2_out ../build/api/openapi --openapiv2_opt logtostderr=true,allow_merge=true api/services/*Service.proto api/services/ObjectLoad.proto
 
 cd ..
 mv build/api/models/* models
